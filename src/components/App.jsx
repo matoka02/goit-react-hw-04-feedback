2e86d65ffead5052038b123bee2855d34ecbaeee
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-19 19:16:32 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,102 +1,73 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // console.log(good, neutral, bad);
+
+  const handleFeedback = evt => {
+    if (evt === 'Good') {
+      setGood(good + 1);
+    } else if (evt === 'Neutral') {
+      setNeutral(neutral + 1);
+    } else if (evt === 'Bad') {
+      setBad(bad + 1);
+    }
   };
 
-  handleFeedback = evt => {
-    // console.log(evt); // evt -- ключ
-    this.setState({
-      [evt]: this.state[evt] + 1,
-    });
-  };
-
-  // // отклонено
-  // handleFeedback = (evt) => {
-  //   console.log(evt);
-  //   if (evt === 'Good') {
-  //     this.setState({good: this.state.good + 1});
-  //   } else if (evt === 'Neutral') {
-  //     this.setState({neutral: this.state.neutral + 1});
-  //   } else if (evt === 'Bad') {
-  //     this.setState({bad: this.state.bad + 1});
-  //   }
-  // };
-
-  totalFeedBack = () => {
-    let total = this.state.good + this.state.neutral + this.state.bad;
+  const totalFeedback = () => {
+    let total = good + neutral + bad;
     return total;
-  };
+  };  
 
-  positivePercentage = () => {
-    if (this.totalFeedBack() === 0) {
+  const positivePercentage = () => {
+    if (totalFeedback() === 0) {
       return 0;
     }
-
-    return Math.round((this.state.good / this.totalFeedBack()) * 100);
+    return Math.round((good / totalFeedback()) * 100);
   };
 
-  render() {
-    return (
-      <div
-        style={{
-          // height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            // // отклонено
-            // options={['Good', 'Neutral', 'Bad']}
-            onLeaveFeedback={this.handleFeedback}
-          />{' '}
-        </Section>
+  return (
+    <div
+      style={{
+        // height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['Good', 'Neutral', 'Bad']}
+          onLeaveFeedback={handleFeedback}
+        />{' '}
+      </Section>
 
-        <Section title="Statistics">
-          {this.totalFeedBack() !== 0 ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.totalFeedBack()}
-              positivePercentage={this.positivePercentage()}
-            />
-          ) : (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
-
-// ==============================================================================================
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
+      <Section title="Statistics">
+        {totalFeedback() !== 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback()}
+            positivePercentage={positivePercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </div>
+  );
+  
+};
